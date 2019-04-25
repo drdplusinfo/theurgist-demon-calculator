e3800f2a300bd7f2e54f0030ac96bddf038056a2
Jaroslav Týc <mail@jaroslavtyc.com>
2019-04-25 09:16:05 +0200

    Few snippets renamed

--- web/parts/demon/demon.php
+++ web/parts/demon/demon.php
@@ -14,23 +14,21 @@ use DrdPlus\Codes\Theurgist\FormulaCode;
   </div>
   <div class="row">
     <div class="col">
-      <select id="formula" name="<?= CurrentDemonValues::DEMON ?>">
-          <?php foreach (FormulaCode::getPossibleValues() as $formulaValue) { ?>
-            <option value="<?= $formulaValue ?>"
-                    <?php if ($formulaValue === $webPartsContainer->getCurrentDemonCode()->getValue()){ ?>selected<?php } ?>>
-                <?= FormulaCode::getIt($formulaValue)->translateTo('cs') ?>
-            </option>
-          <?php } ?>
-      </select>
+      <label>
+        <select id="demon" name="<?= CurrentDemonValues::DEMON ?>">
+            <?php foreach (FormulaCode::getPossibleValues() as $formulaValue) { ?>
+              <option value="<?= $formulaValue ?>"
+                      <?php if ($formulaValue === $webPartsContainer->getCurrentDemonCode()->getValue()){ ?>selected<?php } ?>>
+                  <?= FormulaCode::getIt($formulaValue)->translateTo('cs') ?>
+              </option>
+            <?php } ?>
+        </select>
+      </label>
       <button type="submit">Vybrat</button>
         <?php $formulaDifficulty = $webPartsContainer->getTables()->getDemonsTable()->getDifficulty($webPartsContainer->getCurrentDemonCode()); ?>
       <span>[<?= $formulaDifficulty->getValue() ?>]</span>
-      <span class="forms" title="Forma">
-            <?php $formulaForms = implode(', ', $webPartsContainer->getCurrentDemonValues()->getDemonFormNames($webPartsContainer->getCurrentDemonCode(), 'cs')); ?>
-        (<?= $formulaForms ?>)
-    </span>
     </div>
   </div>
     <?php
-require __DIR__ . '/formula_parameters.php';
-require __DIR__ . '/formula_spell_traits.php';
+require __DIR__ . '/demon_parameters.php';
+require __DIR__ . '/demon_traits.php';
---
+++ web/parts/demon/demon_parameters.php
@@ -0,0 +1,9 @@
+<div class="row">
+  <span class="col"><strong>Parametry</strong>:</span>
+</div>
+<div class="row">
+    <?php
+    include __DIR__ . '/demon_parameters_without_unit.php';
+    include __DIR__ . '/demon_parameters_with_unit.php';
+    ?>
+</div>
