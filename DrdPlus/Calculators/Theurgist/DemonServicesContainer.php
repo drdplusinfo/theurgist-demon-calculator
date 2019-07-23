d0dacb9f7435cbb029d362455726c35fd11b5a7c
Jaroslav Týc <mail@jaroslavtyc.com>
2019-07-23 11:17:33 +0200

    Fixed web root

--- DrdPlus/Calculators/Theurgist/DemonServicesContainer.php
+++ DrdPlus/Calculators/Theurgist/DemonServicesContainer.php
@@ -3,6 +3,7 @@
 namespace DrdPlus\Calculators\Theurgist;
 
 use DrdPlus\CalculatorSkeleton\CalculatorServicesContainer;
+use DrdPlus\RulesSkeleton\Dirs;
 use DrdPlus\RulesSkeleton\Web\WebPartsContainer;
 use DrdPlus\Tables\Tables;
 
@@ -41,5 +42,11 @@ class DemonServicesContainer extends CalculatorServicesContainer
     public function getTables(): Tables
     {
         return Tables::getIt();
+    }
+
+    protected function createRoutedDirs(Dirs $dirs): Dirs
+    {
+        $match = $this->getRulesUrlMatcher()->match($this->getRequest()->getCurrentUrl());
+        return new DemonDirs($dirs->getProjectRoot(), $match->getPath());
     }
 }
